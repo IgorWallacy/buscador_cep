43e8d9d4002c4b98e7309b9fb0d7fb06dd0495bf
Igor Wallacy <doks92@gmail.com>
2022-04-16 12:01:19 -0300

    adicionado consulta de cep e retorno

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 import { InputText } from "primereact/inputtext";
-import { Card } from 'primereact/card';
 
+import { Button } from "primereact/button";
+
+import { Toast } from 'primereact/toast';
+
+
+
+import api from "./service/index";
 
 function App() {
   const [value, setValue] = useState();
+  const [resultado, setResultado] = useState({});
+  const toast = useRef(null);
+
+  const handleSearch = async () => {
+    try {
+       await api
+        .get(`${value}/json`)
+        .then((data) => setResultado(data.data));
+    } catch (error) {
+      toast.current.show({severity:'error', summary: 'Error Message', detail:` Erro ao retornar um cep!   ${error}`, life: 3000});
+    }
+  };
 
   return (
+    <>
+    <Toast ref={toast} position="bottom-center" />
     
-    <Card>
     <div className="container">
       <div className="title">
         <h1>Buscador de CEP</h1>
       </div>
       <div className="containerInput">
-      <span className="p-input-icon-left">
-      <i className="pi pi-search" />
-        <InputText placeholder="Digite um cep ..." className="p-inputtext p-inputtext-lg block" value={value} onChange={(e) => setValue(e.target.value)} />
-      </span>
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            placeholder="Digite um cep ..."
+            className="p-inputtext p-inputtext-lg block"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </span>
       </div>
+
+      <div>
+        <Button
+          className="p-button-rounded"
+          label="Buscar"
+          icon="pi pi-search"
+          onClick={() => handleSearch()}
+        />
+      </div>
+      {Object.keys(resultado).length > 0 && (
+        <div className="resultado">
+          <p>
+            <h2> CEP : {resultado.cep} </h2>
+            <h2> Bairro : {resultado.bairro}</h2>
+            <h2> Complemento :{resultado.complemento}</h2>
+            <h2> DDD: {resultado.ddd}</h2>
+            <h2> GIA : {resultado.gia}</h2>
+            <h2> IBGE : {resultado.ibge}</h2>
+            <h2> LOCALIDADE: {resultado.localidade}</h2>
+            <h2> LOGRADOURO: {resultado.logradouro}</h2>
+            <h2> SIAFI :{resultado.siafi}</h2>
+            <h2> UF :{resultado.uf}</h2>
+          </p>
+        </div>
+      )}
     </div>
-    </Card>
+    </>
   );
 }
 
